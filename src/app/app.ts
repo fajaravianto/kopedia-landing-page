d65a55baf78a6f3b4ca56d445b333a19c0753f46
Fajar <fajaravianto2@gmail.com>
2026-05-12 11:49:53 +0800

    Update: Reformat section digital products 1.0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Navbar } from './components/navbar/navbar'; 
 import { Hero } from './components/hero/hero'; 
 // 1. Import fungsi registrasi Swiper
@@ -10,7 +10,7 @@ import AOS from 'aos';
 register();
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -24,6 +24,41 @@ export class App implements OnInit { // ⬅️ IMPLEMENTS ONINIT
       duration: 1000, // Durasi animasi 1 detik
       once: true,     // Animasi hanya jalan sekali saat di-scroll turun
       offset: 100,    // Animasi mulai 100px sebelum elemen terlihat
-    });
+    });  
   }
+ digitalProducts = [
+    {
+      id: 'simantek',
+      name: 'Simantek Biller System',
+      logo: '/simantek-logo.png', 
+      desc: 'Simantek is a cloud-based platform that provides mobile phone credit server services and serves as a solution for digital product businesses. With a fast, secure, and efficient system, Simantek helps entrepreneurs manage digital product transactions easily.'
+    },
+    {
+      id: 'irs',
+      name: 'Integrated Reload System',
+      logo: '/irs-logo.png',
+      desc: 'A reliable, high-transaction management solution. Handles mobile phone data traffic with maximum stability through an integrated backup-restore module and automated terminal.'
+    },
+    {
+      id: 'irsmarket',
+      name: 'IRS Market',
+      logo: '/irsmarket-logo.png',
+      desc: 'B2B Digital Marketplace. A new era of secure and trusted H2H transactions. Connecting reseller and supplier ecosystems in a single, integrated portal for efficient national digital product distribution.'
+    },
+    {
+      id: 'digicore',
+      name: 'DigiCore',
+      logo: '/digicore-logo.png',
+      desc: 'Core infrastructure for thousands of digital product services. Providing competitive pricing without compromising on the quality of technical support to help our partners businesses thrive.'
+    }
+  ];
+
+  // 2. State untuk melacak produk yang sedang diklik (Default: Simantek)
+  activeProduct = this.digitalProducts[0];
+
+  // 3. Fungsi yang dipanggil saat logo di klik
+  selectProduct(product: any) {
+    this.activeProduct = product;
+  }
+ 
 }
