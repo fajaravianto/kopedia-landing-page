8e709995fe5d5016528d95e586a13d5253eb504d
Fajar <fajaravianto2@gmail.com>
2026-05-13 11:27:34 +0800

    feat: redesign ecosystem page, add privacy policy routing, and optimize footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy';
+import { EcosystemComponent } from './ecosystem/ecosystem'; // Tambahkan ini
 
 export const routes: Routes = [
   // 1. Jika URL kosong (localhost:4200 / domain utama), arahkan ke Landing Page
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   
   // 3. Fallback (jika user mengetik URL ngawur, kembalikan ke Home)
+  { path: 'ecosystem', component: EcosystemComponent },
   { path: '**', redirectTo: '' }
 ];
